1bf862cd62edef0e0428ae5f1edfdb6833d96f57
Henrik Hallenberg <HNB@energinet.dk>
2024-02-25 22:16:14 +0100

    Start hellow

---
+++ TimeRegistration/TimeRegistration.Client/Services/GeolocationService.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.JSInterop;
+
+namespace TimeRegistration.Client.Services;
+
+public class GeolocationService
+{
+    private Task<IJSObjectReference> module {  get; init; }
+
+    public GeolocationService(IJSRuntime jsRuntime)
+    {
+        module = jsRuntime.InvokeAsync<IJSObjectReference>("import", "./_content/TimeRegistration.Client/Services/GeolocationService.cs.js").AsTask();
+    }
+
+    public async Task GetGeolocationAsync()
+    {
+        var test = await (await module).InvokeAsync<(double, double)>("GetGeolocation");
+    }
+}
---
+++ TimeRegistration/TimeRegistration.Client/Services/GeolocationService.cs.js
@@ -0,0 +1,15 @@
+﻿function GetGeolocation() {
+    let position
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    }
+    else {
+        return null;
+    }
+    return position
+}
+
+function showPosition(position) {
+    position.latitude = position.coords.latitude;
+    position.longitude = position.coords.longitude;
+}
