55bb24f7971e82af0dd3fda3b0053ceec701e5f0
Henrik Hallenberg <HNB@energinet.dk>
2024-03-08 00:50:28 +0100

    Start scripting

---
+++ .github/Powrshell/Setup/SetupGitHooks.psm1
@@ -0,0 +1,9 @@
+﻿function SetupGitHooks
+{
+	Write-Host "Start Setup Git Hooks"
+}
+
+function GetGitHools
+{
+	Get-ChildItem -Path "$PSScriptRoot\..\GitHooks\" -Filter "*.ps1"
+}
---
+++ .github/Powrshell/SetupProject.ps1
@@ -0,0 +1,10 @@
+﻿
+
+Remove-Item -Path "$PSScriptRoot\..\bin" -Force
+dotnet build ".\.github\Setup.csproj" --configuration Release --force 
+
+$ExicutionPath = Resolve-Path -Path "$PSScriptRoot\..\bin\Release\*\Powrshell"
+
+Import-Module "$ExicutionPath\Setup\SetupGitHooks.psm1"
+
+SetupGitHooks
