7706636887c528d5a10a4036e9eaf209e5e202e5
Henrik Hallenberg <HNB@energinet.dk>
2024-03-10 22:17:30 +0100

    Addet tests

---
+++ TimeRegistration.Client.Test/Unit/Test.cs
@@ -0,0 +1,25 @@
+﻿using FluentAssertions;
+
+namespace TimeRegistration.Client.Test.Unit;
+public class Test
+{
+    [Fact]
+    public void SucitingTest()
+    {
+        // Act
+        bool test = true;
+
+        // Assert
+        test.Should().BeTrue();
+    }
+    [Fact]
+    public void FailingTest()
+    {
+        // Act
+        bool test = false;
+
+        // Assert
+        test.Should().BeTrue();
+
+    }
+}
