59293c92cc2393c611aa21fc9437bdd33afb219e
Henrik Hallenberg <HNB@energinet.dk>
2024-03-10 21:51:23 +0100

    Add .NET test workflow and update main deployment workflow

--- .github/Powrshell/GitHooks/pre-commit.ps1
+++ .github/Powrshell/GitHooks/pre-commit.ps1
@@ -1,11 +1,11 @@
 ﻿
+Write-Host "Script was run"
 dotnet build ".\.github\Setup.csproj" --configuration Release --no-incremental --force
 
 $ExicutionPath = Resolve-Path -Path "$PSScriptRoot\..\CompileWorkflows\copyWorkflow.psm1"
 
 Import-Module $ExicutionPath -force
 CompileWorkfolw
-Write-Host "Script was run"
 
 Start-Sleep 20
 Write-Host "Script complidet"
