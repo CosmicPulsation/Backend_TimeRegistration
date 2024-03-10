b57f474ca8c36398c5a47e07924383ae7e392aeb
Henrik Hallenberg <HNB@energinet.dk>
2024-03-11 00:19:19 +0100

    Addet bunit test.

--- TimeRegistration.Client.Test/Unit/Test.cs
+++ TimeRegistration.Client.Test/Unit/Test.cs
@@ -1,4 +1,6 @@
-﻿using FluentAssertions;
+﻿using Bunit;
+using FluentAssertions;
+using TimeRegistration.Client.Pages;
 
 namespace TimeRegistration.Client.Test.Unit;
 public class Test
@@ -11,5 +13,22 @@ public class Test
 
         // Assert
         test.Should().BeTrue();
+    }
+
+    [Fact]
+    public void Homepage()
+    {
+        // Arrage
+        TestContext context = new();
+        var render = context.RenderComponent<Home>();
+        var button = render.Find("[data-testid=\"bigin time registration\"]");
+
+        // Act
+        button.Click();
+
+
+        // Assert
+        var Status = render.Find("[data-testid=\"Status indicator\"]");
+        Status.TextContent.Should().BeEquivalentTo("Status: Working");
     }
 }
---
+++ TimeRegistration/TimeRegistration.Client/Pages/Home.razor.cs
@@ -0,0 +1,12 @@
+﻿namespace TimeRegistration.Client.Pages;
+
+partial class Home
+{
+    bool Bigin { get; set; } = false;
+
+    string Status => Bigin ? "Working" : "Stopt";
+    public void onBiginTimeRegistration()
+    {
+        Bigin = true;
+    }
+}
